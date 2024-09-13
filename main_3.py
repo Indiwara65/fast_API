39e469fb168b36ed18f001d9164368fd82c91374
Indiwara <indiwarananayakkara1@gmail.com>
2024-09-13 21:41:33 +0530

    Response Model

--- main_3.py
+++ main_3.py
@@ -1,13 +1,14 @@
 import pprint
 from typing import Annotated
-from fastapi import FastAPI, Query, Path , Body
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, Path , Body, Cookie
+from pydantic import BaseModel, Field
 
+##Field can be used to define metadata inside a request body defined using BaseModel##
 class Item(BaseModel):
     name:str
-    description:str|None=None
-    price:float
-    tax:float
+    description:str|None=Field(default=None, max_length=50, title="Small Description About the Item")
+    price:float=Field(gt=0,lt=100)
+    tax:float=Field(ge=0,le=100,default=0)
 
 items = {
     "0":Item(name="Soda",description="Breverage",price=1.25,tax=0.02),
@@ -44,10 +45,18 @@ async def item_discount(item_1:Item, item_2:Item, dis_rate:Annotated[float, Body
     discount = total*dis_rate
     return {"total":total, "discount":discount}
 
-##Receive 
+##Receive as JSON
 @app.put("/items/tax_rate")
 async def cal_tax_rate(item:Annotated[Item, Body(embed=True)]):
     return round(item.tax/item.price, 2)
 
-
-
+########## Cookies ##########
+@app.put("/items/description/")
+async def get_description(item_id:Annotated[str, Cookie()]):
+    print(f"Cookie:{item_id}")
+    print(type(item_id))
+    if item_id in items.keys():
+        description = items[item_id].description
+    else:
+        description = "Invalid Id"
+    return description
---
+++ main_4.py
@@ -0,0 +1,48 @@
+from typing import Annotated, List
+from fastapi import FastAPI, Body, Query
+from pydantic import BaseModel, Field
+
+class Item(BaseModel):
+    name : str=Field(max_length=10)
+    description : str|None=Field(None,max_length=50)
+    price:float
+    tax_rate:float=Field(ge=0,lt=1)
+
+class Order(BaseModel):
+    order_Id : int
+    cost:float
+    taxes:float
+    total:float
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Price Listings"
+
+## Response Model ##
+#Method 1
+@app.put("/items/total")
+async def total(items:Annotated[List[Item], Body()], order_Id:Annotated[int, Query()]) -> Order:
+    tot_price = 0
+    tot_tax = 0
+    for item in items:
+        tot_price += item.price
+        tot_tax += item.price*item.tax_rate
+    order = Order(order_Id=order_Id, cost=tot_price, taxes=tot_tax, total=tot_price+tot_tax)
+    return order
+#Method 2
+#response_model - define the data structure of the response
+#response_model_exclude - exclude certain feilds of the data structure
+#response_model_include - include certain feilds of the data structue
+@app.put("/item/price", response_model=Order, response_model_exclude={"cost"}, response_model_include={"total"})
+async def total_price(items:Annotated[List[Item], Body()], order_Id:Annotated[int, Query()]):
+    tot_price = 0
+    tot_tax = 0
+    for item in items:
+        tot_price += item.price
+        tot_tax += item.price*item.tax_rate
+    order = Order(order_Id=order_Id, cost=tot_price, taxes=tot_tax, total=tot_price+tot_tax)
+    return order
+
+
