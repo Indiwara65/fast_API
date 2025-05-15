62b6e09e4bf03ad7822204eb1ba4ecbadd4de0c6
Indiwara65 <indiwarananayakkara1@gmail.com>
2025-05-15 13:48:29 +0530

    path parameters

--- main_4.py
+++ main_4.py
@@ -35,11 +35,12 @@ async def total(items:Annotated[List[Item], Body()], order_Id:Annotated[int, Que
 #response_model - define the data structure of the response
 #response_model_exclude - exclude certain feilds of the data structure
 #response_model_include - include certain feilds of the data structue
+#status_code - can be used to set the http status code manually 
 @app.put("/item/price", response_model=Order, response_model_exclude={"cost"}, response_model_include={"total"})
 async def total_price(items:Annotated[List[Item], Body()], order_Id:Annotated[int, Query()]):
     tot_price = 0
     tot_tax = 0
-    for item in items:
+    for item in items: 
         tot_price += item.price
         tot_tax += item.price*item.tax_rate
     order = Order(order_Id=order_Id, cost=tot_price, taxes=tot_tax, total=tot_price+tot_tax)
---
+++ main_5.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from fastapi import FastAPI
+
+class ChannelName(str, Enum):
+    Cleet = "Cleetus McFarland"
+    JH = "JH Diesel & 4x4"
+    Matt = "Matts Offroad Recovery"
+
+app = FastAPI()
+names = {'1':"JH","2":"Cleet","3":"JackStand"}
+
+@app.get("/")
+def root():
+    return {"message":"Hello World"}
+
+@app.get("/main")
+def main_path():
+    return {"message":"This is the main path"}
+
+@app.get("/who/name/{name}")
+def who(name:str):
+    if name.lower()=='jh':
+        return {"message":"JH Who"}
+    else:
+        return {"message":name}  
+    
+@app.get("/who/id/{name_id}")
+def who(name_id:int):
+    name = names.get(str(name_id))
+    print(name)
+    if name is None:
+        return {"message":"no entry"}
+    elif name.lower() == 'jh':
+        return {'message':"JH who"}
+    else:
+        return {"message":name}
+    
+@app.get("/channel/{channel_name}")
+def channelName(channel_name:ChannelName):
+    if channel_name==ChannelName.Cleet:
+        owner = "Garret Mitchel"
+    elif channel_name==ChannelName.JH:
+        owner = "Justin hilbilly"
+    elif channel_name==ChannelName.Matt:
+        owner = "Matt Westlin"
+    else:
+        owner = "Not defined"
+    return {'owner':owner}
